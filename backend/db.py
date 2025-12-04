b87ddeb50f093737ca0a4e9f654888de4aed99b0
Taylor Yee <tayloryee1@gmail.com>
2025-12-04 15:39:14 -0800

    created data model

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,16 @@
 # db.py
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,declarative_base
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/brain_dump"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
+Base =declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
