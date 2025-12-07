3f743319f1addfbe81e39429e580d38893a08b1f
Taylor Yee <tayloryee@Taylors-MacBook-Pro-2.local>
2025-12-07 15:21:01 -0800

    fixed celery_app

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -4,8 +4,7 @@ from celery import Celery
 celery_app = Celery(
     "brain_dump",
     broker="redis://redis:6379/0",#This tells Celery where your task queue lives (Redis)
-    backend="redis://redis:6379/1",  # optional, for storing results
-    include=["app.worker.tasks"],
+    backend="redis://redis:6379/1"  # optional, for storing results
 )
 
 
